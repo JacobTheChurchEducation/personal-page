49c1b104b8f22c193b350f209a544aa9a0b6a770
JacobTheChurch <j.m.kosciolek@student.utwente.nl>
2022-08-12 14:21:01 +0200

    Completed index.html

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,4 @@
-const navJS = () => {
-
-
-
+const mNavbar = () => {
 
     const nav = document.querySelector('.nav-icon');
     const navWrapper = document.querySelector('.nav-wrapper');
@@ -10,8 +7,9 @@ const navJS = () => {
     const section2 = document.querySelector('#MyGols')
     const listItems = document.querySelectorAll('.nav-bar ul li');
     const container = document.querySelector("#container");
-    const element = document.querySelector(".picked");
-    const dot = window.getComputedStyle(element, '::after');
+    const picked = document.querySelector(".picked");
+
+
 
 
 
@@ -26,10 +24,6 @@ const navJS = () => {
     });
 
     container.addEventListener('scroll', () => {
-        // alert(header.clientHeight);
-        // alert("Section:" + section1.clientHeight)
-
-        // alert(section2.getBoundingClientRect().bottom);
 
         if(section1.getBoundingClientRect().bottom <= 0 && section2.getBoundingClientRect().bottom > 0) {
             header.classList.add('dark');
@@ -37,7 +31,8 @@ const navJS = () => {
             document.querySelector('.line2').classList.add('dark-element');
             document.querySelector('.line3').classList.add('dark-element');
             document.querySelector('nav ul').classList.add('dark');
-            element.classList.add('dark-element-dot');
+            picked.classList.add('dark-element-dot');
+
 
             for(let i in listItems) {
                listItems[i].classList.add('dark-element-font');
@@ -50,20 +45,88 @@ const navJS = () => {
             document.querySelector('.line3').classList.remove('dark-element');
             document.querySelector('nav ul').classList.remove('dark');
             document.querySelector('nav ul li a').classList.remove('dark-element-font');
-            element.classList.remove('dark-element-dot');
+            picked.classList.remove('dark-element-dot');
 
             for(let i in listItems) {
                 listItems[i].classList.remove('dark-element-font');
             }
 
-
-            document.querySelector('.picked::after').style.backgroundColor = "#212121";
         }
-
     })
+}
+    // const dot = document.querySelector('#dot');
+
+const navBarScrollColorChange = () =>
+{
 
 }
 
 
+const navBarDot = () =>
+{
 
-navJS();
+    const home = document.querySelector('#home');
+    const blog = document.querySelector('#blog');
+    const myWork = document.querySelector('#myWork');
+    const picked = document.querySelector(".picked");
+
+
+    home.addEventListener('mouseover', function(){dotIn(home)});
+        blog.addEventListener('mouseover', function(){dotIn(blog)});
+        myWork.addEventListener('mouseover', function(){dotIn(myWork)});
+
+        home.addEventListener('mouseout', dotOut);
+        blog.addEventListener('mouseout', dotOut);
+        myWork.addEventListener('mouseout', dotOut);
+
+
+
+
+
+    function dotIn(li) {
+        // const left = li.getBoundingClientRect().x + (li.clientWidth / 2 - 3);
+
+        if(li !== picked) {
+        const right = li.getBoundingClientRect().x + (li.clientWidth / 2 + 3) - picked.getBoundingClientRect().x - (picked.clientWidth);
+        // console.log("right: " + right);
+        // console.log("li x: " + li.getBoundingClientRect().x)
+        //
+        // console.log("picked x: " +  picked.getBoundingClientRect().x)
+        //
+        // console.log("picked width: " + (picked.clientWidth / 2 - 3 ))
+
+        // picked.classList.add('.blog');
+
+        // alert(window.getComputedStyle(picked, '::after').backgroundColor)
+
+        // picked.style.setProperty('--leftChange', 'initial');
+        picked.style.setProperty('--rightChange', '-' + right.toString() + 'px');
+        // picked.style.setProperty('--leftChange', '5345435px');
+        // document.querySelector('.blog::after').style.left = left.toString();
+
+        }
+
+    }
+    function dotOut()
+    {
+
+        // picked.style.setProperty('--leftChange', 'calc(50% - 3px)');
+        picked.style.setProperty('--rightChange', 'calc(50% - 3px)');
+    }
+}
+
+const blogPage = () =>
+{
+    mNavbar();
+    navBarDot();
+}
+
+const mainPage = () =>
+{
+    mNavbar()
+    navBarDot()
+}
+
+// navJS();
+
+
