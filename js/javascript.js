7a3673bbe983047a8e88a0827161092b7a006f36
JacobTheChurchEducation <j.m.kosciolek@student.utwente.nl>
2024-01-14 09:16:10 +0100

    yet to be finished

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,22 @@
-const mNavbar = () => {
+const decideColor = () => {
+
+    alert("decideClor")
+    alert(window.location.pathname)
+
+    if(window.location.pathname.split(".html")[1] === "myWork")
+    {
+        alert("if spk")
+        document.documentElement.style.setProperty("--main-color", "#212121");
+        document.documentElement.style.setProperty("--main-font-color", "#F8F8F8");
+    }
+
+}
+
+
+const mNavbarAppearDisAppear = () => {
 
     const nav = document.querySelector('.nav-icon');
     const navWrapper = document.querySelector('.nav-wrapper');
-    const header = document.querySelector('.nav-bar');
-    const section1 = document.querySelector('#AboutMe')
-    const section2 = document.querySelector('#MyGols')
-    const listItems = document.querySelectorAll('.nav-bar ul li');
-    const container = document.querySelector("#container");
-    const picked = document.querySelector(".picked");
-
 
 
 
@@ -23,41 +31,50 @@ const mNavbar = () => {
         nav.classList.toggle('toggle');
     });
 
-    container.addEventListener('scroll', () => {
 
-        if(section1.getBoundingClientRect().bottom <= 0 && section2.getBoundingClientRect().bottom > 0) {
-            header.classList.add('dark');
-            document.querySelector('.line1').classList.add('dark-element');
-            document.querySelector('.line2').classList.add('dark-element');
-            document.querySelector('.line3').classList.add('dark-element');
-            document.querySelector('nav ul').classList.add('dark');
-            picked.classList.add('dark-element-dot');
-
-
-            for(let i in listItems) {
-               listItems[i].classList.add('dark-element-font');
-            }
-        }
-        else {
-            header.classList.remove('dark');
-            document.querySelector('.line1').classList.remove('dark-element');
-            document.querySelector('.line2').classList.remove('dark-element');
-            document.querySelector('.line3').classList.remove('dark-element');
-            document.querySelector('nav ul').classList.remove('dark');
-            document.querySelector('nav ul li a').classList.remove('dark-element-font');
-            picked.classList.remove('dark-element-dot');
-
-            for(let i in listItems) {
-                listItems[i].classList.remove('dark-element-font');
-            }
-
-        }
-    })
 }
     // const dot = document.querySelector('#dot');
 
-const navBarScrollColorChange = () =>
+const mNavBarScrollColorChange = () =>
 {
+const header = document.querySelector('.nav-bar');
+const section1 = document.querySelector('#AboutMe')
+const section2 = document.querySelector('#MyGols')
+const listItems = document.querySelectorAll('.nav-bar ul li');
+const container = document.querySelector("#container");
+const picked = document.querySelector(".picked");
+
+
+   container.addEventListener('scroll', () => {
+
+    if(section1.getBoundingClientRect().bottom <= 0 && section2.getBoundingClientRect().bottom > 0) {
+        header.classList.add('dark');
+        document.querySelector('.line1').classList.add('dark-element');
+        document.querySelector('.line2').classList.add('dark-element');
+        document.querySelector('.line3').classList.add('dark-element');
+        document.querySelector('nav ul').classList.add('dark');
+        picked.classList.add('dark-element-dot');
+
+
+        for(let i in listItems) {
+            listItems[i].classList.add('dark-element-font');
+        }
+    }
+    else {
+        header.classList.remove('dark');
+        document.querySelector('.line1').classList.remove('dark-element');
+        document.querySelector('.line2').classList.remove('dark-element');
+        document.querySelector('.line3').classList.remove('dark-element');
+        document.querySelector('nav ul').classList.remove('dark');
+        document.querySelector('nav ul li a').classList.remove('dark-element-font');
+        picked.classList.remove('dark-element-dot');
+
+        for(let i in listItems) {
+            listItems[i].classList.remove('dark-element-font');
+        }
+
+    }
+})
 
 }
 
@@ -72,12 +89,12 @@ const navBarDot = () =>
 
 
     home.addEventListener('mouseover', function(){dotIn(home)});
-        blog.addEventListener('mouseover', function(){dotIn(blog)});
-        myWork.addEventListener('mouseover', function(){dotIn(myWork)});
+    blog.addEventListener('mouseover', function(){dotIn(blog)});
+    myWork.addEventListener('mouseover', function(){dotIn(myWork)});
 
-        home.addEventListener('mouseout', dotOut);
-        blog.addEventListener('mouseout', dotOut);
-        myWork.addEventListener('mouseout', dotOut);
+    home.addEventListener('mouseout', dotOut);
+    blog.addEventListener('mouseout', dotOut);
+    myWork.addEventListener('mouseout', dotOut);
 
 
 
@@ -87,20 +104,20 @@ const navBarDot = () =>
         // const left = li.getBoundingClientRect().x + (li.clientWidth / 2 - 3);
 
         if(li !== picked) {
-        const right = li.getBoundingClientRect().x + (li.clientWidth / 2 + 3) - picked.getBoundingClientRect().x - (picked.clientWidth);
-        // console.log("right: " + right);
-        // console.log("li x: " + li.getBoundingClientRect().x)
-        //
-        // console.log("picked x: " +  picked.getBoundingClientRect().x)
-        //
-        // console.log("picked width: " + (picked.clientWidth / 2 - 3 ))
+        const right =( li.getBoundingClientRect().x + (li.clientWidth / 2 + 3) - picked.getBoundingClientRect().x - (picked.clientWidth) )*(-1);
+        console.log("right: " + right);
+        console.log("li x: " + li.getBoundingClientRect().x)
 
-        // picked.classList.add('.blog');
+        console.log("picked x: " +  picked.getBoundingClientRect().x)
+
+        console.log("picked width: " + (picked.clientWidth / 2 - 3 ))
+
+        picked.classList.add('.blog');
 
         // alert(window.getComputedStyle(picked, '::after').backgroundColor)
 
-        // picked.style.setProperty('--leftChange', 'initial');
-        picked.style.setProperty('--rightChange', '-' + right.toString() + 'px');
+        picked.style.setProperty('--leftChange', 'initial');
+        picked.style.setProperty('--rightChange', + right.toString() + 'px');
         // picked.style.setProperty('--leftChange', '5345435px');
         // document.querySelector('.blog::after').style.left = left.toString();
 
@@ -115,18 +132,18 @@ const navBarDot = () =>
     }
 }
 
-const blogPage = () =>
+
+
+const start = () =>
 {
-    mNavbar();
+    mNavbarAppearDisAppear();
     navBarDot();
 }
 
-const mainPage = () =>
+const main = () =>
 {
-    mNavbar()
-    navBarDot()
+    mNavbarAppearDisAppear();
+    mNavBarScrollColorChange();
+    navBarDot();
 }
-
-// navJS();
-
 
